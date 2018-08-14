e1ef3e7b60908912f0aa089fd947ab3a54958ad8
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-14 09:06:23 -0700

    add total functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Cart.css'
 import CartItems from '../CartItems/CartItems';
 
-export default (props) => (
+export default (props) => {
+    let total = props.cart.reduce((tot, item) => tot + parseFloat(item.product.price.replace('$', '')) * item.quantity, 0);
+    return (
     <section className="Cart">
         Cart Items
         <CartItems 
         cart={props.cart}
         handleRemoveItem={props.handleRemoveItem}
         />
+        <div>Total: {total} </div>
     </section>
-);
+    );
+};
